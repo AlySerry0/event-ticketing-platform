70ec7570f739e33dc9932d500928ab47a61fe68f
Chromiks <chrimerali@gmail.com>
2026-04-30 01:46:31 +0300

    feat(ticket): S4-F10 Get Ticket Analytics Dashboard (55-5288)

---
+++ ticket-service/src/main/java/com/team7/eventticketing/ticket/adapter/TicketAnalyticsAdapter.java
@@ -0,0 +1,4 @@
+package com.team7.eventticketing.ticket.adapter;
+
+public class TicketAnalyticsAdapter {
+}
---
+++ ticket-service/src/main/java/com/team7/eventticketing/ticket/dto/TicketAnalyticsDTO.java
@@ -0,0 +1,4 @@
+package com.team7.eventticketing.ticket.dto;
+
+public class TicketAnalyticsDTO {
+}
