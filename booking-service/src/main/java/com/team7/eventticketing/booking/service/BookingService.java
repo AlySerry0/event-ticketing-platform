8061145e289ae872c957012216cc9604d63f6f78
Chromiks <chrimerali@gmail.com>
2026-04-29 23:58:26 +0300

    fix(booking): add missing CopyOnWriteArrayList import (55-5288)

--- booking-service/src/main/java/com/team7/eventticketing/booking/service/BookingService.java
+++ booking-service/src/main/java/com/team7/eventticketing/booking/service/BookingService.java
@@ -29,6 +29,8 @@ import com.team7.eventticketing.booking.observer.EntitySubject;
 import com.team7.eventticketing.booking.observer.MongoEventLogger;
 import com.team7.eventticketing.booking.util.CacheInvalidationService;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 @Service
 public class BookingService implements EntitySubject {
 
