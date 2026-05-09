001aa41011811c775ad216016309e656720ac447
Abdelrahman Elaby <abdelrahmanelaby@gmail.com>
2026-05-09 19:25:20 +0300

    feat(contracts): create open feign clients (55-0990)

---
+++ contracts/src/main/java/com/team7/eventticketing/contracts/clients/EventServiceClient.java
@@ -0,0 +1,12 @@
+package com.team7.eventticketing.contracts.clients;
+
+import com.team7.eventticketing.contracts.dto.*;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "event-service", url = "${services.event.url:http://event-service:8080}")
+public interface EventServiceClient {
+
+}
