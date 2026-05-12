737ef43f25cdd096379bd1d0ff2e55f4fdd42d7c
Abdelrahman Elaby <abdelrahmanelaby@gmail.com>
2026-05-12 11:05:20 +0300

    fix(contracts): fix clients urls (55-0990)

--- contracts/src/main/java/com/team7/eventticketing/contracts/clients/SalesServiceClient.java
+++ contracts/src/main/java/com/team7/eventticketing/contracts/clients/SalesServiceClient.java
@@ -3,7 +3,7 @@ package com.team7.eventticketing.contracts.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "sales-service", url = "${services.sales.url:http://sales-service:8080}")
+@FeignClient(name = "sales-service", url = "${feign.sales-service.url:http://sales-service:8080}")
 public interface SalesServiceClient {
 
 }
