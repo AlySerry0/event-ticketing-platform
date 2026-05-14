7862a73306f5675fd27e1a3c8417031bf9198c21
Zeina Fadel <zeina.abdeen@student.guc.edu.eg>
2026-05-14 22:45:38 +0300

    feat(event): add S2 events messaging dependencies (55-1095)

--- event-service/src/main/java/com/team7/eventticketing/event/EventServiceApplication.java
+++ event-service/src/main/java/com/team7/eventticketing/event/EventServiceApplication.java
@@ -2,8 +2,11 @@ package com.team7.eventticketing.event;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.team7.eventticketing")
 public class EventServiceApplication {
 
     public static void main(String[] args) {
