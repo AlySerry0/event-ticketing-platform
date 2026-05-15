84cd6abb08b8225a067b1fe9b7d3f9acb1afe3f6
Abdelrahman Elaby <abdelrahmanelaby@gmail.com>
2026-05-15 10:17:27 +0300

    fix(contracts): clients package rename (55-0990)

--- contracts/src/main/java/com/team7/eventticketing/contracts/feign/BookingServiceClient.java
+++ contracts/src/main/java/com/team7/eventticketing/contracts/feign/BookingServiceClient.java
@@ -1,9 +1,6 @@
-package com.team7.eventticketing.contracts.clients;
+package com.team7.eventticketing.contracts.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @FeignClient(name = "booking-service", url = "${feign.booking-service.url:http://booking-service:8080}")
 public interface BookingServiceClient {
--- contracts/src/main/java/com/team7/eventticketing/contracts/feign/SalesServiceClient.java
+++ contracts/src/main/java/com/team7/eventticketing/contracts/feign/SalesServiceClient.java
@@ -1,7 +1,6 @@
-package com.team7.eventticketing.contracts.clients;
+package com.team7.eventticketing.contracts.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "sales-service", url = "${feign.sales-service.url:http://sales-service:8080}")
 public interface SalesServiceClient {
