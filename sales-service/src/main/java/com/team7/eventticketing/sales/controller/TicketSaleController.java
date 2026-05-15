9b6a51e22b65434194c00dae0217c9104c7aa892
mariamloutfy <maryamloutfy27@gmail.com>
2026-05-15 23:01:03 +0300

    Update TicketSaleController.java
    feat(sales): remove duplicate import (55-1885)

--- sales-service/src/main/java/com/team7/eventticketing/sales/controller/TicketSaleController.java
+++ sales-service/src/main/java/com/team7/eventticketing/sales/controller/TicketSaleController.java
@@ -16,7 +16,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/sales")
